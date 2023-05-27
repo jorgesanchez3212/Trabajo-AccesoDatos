9ba5ec53a30f20fbeab3756cb0030abfad36c2ba
Jorge <jsanchezberrocoso@gmail.com>
2023-05-27 21:11:35 +0200

    Cache

---
+++ TrabajoSQL/src/main/kotlin/services/citas/CitaCache.kt
@@ -0,0 +1,28 @@
+package services.citas
+
+
+import io.github.reactivecircus.cache4k.Cache
+import kotlinx.coroutines.*
+import models.Cita
+import kotlin.time.Duration.Companion.minutes
+
+private const val STOP = 6 * 10000L
+
+class CitaCache {
+
+    val cache = Cache.Builder()
+        .expireAfterWrite(1.minutes)
+        .expireAfterAccess(1.minutes)
+        .build<String, Cita>()
+
+    suspend fun refresh() {
+        withContext(newSingleThreadContext("cache")) {
+            launch {
+                println("ACTUALIZANDO CACHE")
+                cache.invalidateAll()
+                delay(STOP)
+            }
+        }
+    }
+
+}
---
+++ TrabajoSQL/src/main/kotlin/services/informes/InformeCache.kt
@@ -0,0 +1,29 @@
+package services.informes
+
+
+import io.github.reactivecircus.cache4k.Cache
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
+import models.Informe
+import kotlin.time.Duration.Companion.minutes
+
+private const val STOP = 6 * 10000L
+
+class InformeCache {
+    val cache = Cache.Builder()
+        .expireAfterWrite(1.minutes)
+        .expireAfterAccess(1.minutes)
+        .build<String, Informe>()
+
+    suspend fun refresh() {
+        withContext(newSingleThreadContext("cache")) {
+            launch {
+                println("ACTUALIZANDO CACHE")
+                cache.invalidateAll()
+                delay(STOP)
+            }
+        }
+    }
+}
